6c05784cf51bf009982a9729dc090f055e0bc586
04lxshyak <04lakshyak@gmail.com>
2025-07-21 11:45:37 +0530

    initial commit

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,31 +1,23 @@
 package net.engineeringdigest.journalApp.entity;
 
+import lombok.*;
+import net.engineeringdigest.journalApp.enums.Sentiment;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
+@Data
+@NoArgsConstructor
 public class JournalEntry {
-    private long id;
+    @Id
+    private ObjectId id;
+    @NonNull
     private String title;
     private String content;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    private LocalDateTime date;
+    private Sentiment sentiment;
 }
